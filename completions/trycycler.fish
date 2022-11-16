3ab18def946caa18cca6c2aefec047baa5a43b49
Yamato Matsuoka <yamaton@gmail.com>
2022-11-16 12:34:44 -0500

    Update unicycler trycycler

--- completions/trycycler.fish
+++ completions/trycycler.fish
@@ -1,7 +1,7 @@
 # Auto-generated with h2o
 
-complete -c trycycler -n "not __fish_seen_subcommand_from subsample cluster reconcile msa partition consensus" -s "h" -l "help" -d "Show this help message and exit"
-complete -c trycycler -n "not __fish_seen_subcommand_from subsample cluster reconcile msa partition consensus" -l "version" -d "Show program's version number and exit"
+complete -c trycycler -n "not __fish_seen_subcommand_from subsample cluster dotplot reconcile msa partition consensus" -s "h" -l "help" -d "Show this help message and exit"
+complete -c trycycler -n "not __fish_seen_subcommand_from subsample cluster dotplot reconcile msa partition consensus" -l "version" -d "Show program's version number and exit"
 
 
 
@@ -9,6 +9,7 @@ complete -k -c trycycler -n __fish_use_subcommand -x -a consensus -d "derive a c
 complete -k -c trycycler -n __fish_use_subcommand -x -a partition -d "partition reads by cluster"
 complete -k -c trycycler -n __fish_use_subcommand -x -a msa -d "multiple sequence alignment"
 complete -k -c trycycler -n __fish_use_subcommand -x -a reconcile -d "reconcile contig sequences"
+complete -k -c trycycler -n __fish_use_subcommand -x -a dotplot -d "draw pairwise dotplots for a cluster"
 complete -k -c trycycler -n __fish_use_subcommand -x -a cluster -d "cluster contigs by similarity"
 complete -k -c trycycler -n __fish_use_subcommand -x -a subsample -d "subsample a long-read set"
 
@@ -19,7 +20,7 @@ complete -c trycycler -n "__fish_seen_subcommand_from subsample" -s "o" -l "out_
 complete -c trycycler -n "__fish_seen_subcommand_from subsample" -l "count" -d "Number of subsampled read sets to output (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from subsample" -l "genome_size" -d "Approximate genome size (default: auto)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from subsample" -l "min_read_depth" -d "Minimum allowed read depth (default: 25.0)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from subsample" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from subsample" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from subsample" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from subsample" -l "version" -d "Show program's version number and exit"
 
@@ -31,16 +32,24 @@ complete -c trycycler -n "__fish_seen_subcommand_from cluster" -s "o" -l "out_di
 complete -c trycycler -n "__fish_seen_subcommand_from cluster" -l "min_contig_len" -d "Exclude contigs shorter than this many base pairs in length (default: 1000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from cluster" -l "min_contig_depth" -d "Exclude contigs with less than this much read depth relative to the mean read depth (default: 0.1)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from cluster" -l "distance" -d "Mash distance complete-linkage clustering threshold (default: 0.01)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from cluster" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from cluster" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from cluster" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from cluster" -l "version" -d "Show program's version number and exit"
+
+
+
+complete -c trycycler -n "__fish_seen_subcommand_from dotplot" -s "c" -l "cluster_dir" -d "Cluster directory (should contain a 1_contigs subdirectory)" -r
+complete -c trycycler -n "__fish_seen_subcommand_from dotplot" -l "kmer" -d "K-mer size to use in dot plots (default: 32)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from dotplot" -l "res" -d "Size (in pixels) of each dot plot image (default: 2000)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from dotplot" -s "h" -l "help" -d "Show this help message and exit"
+complete -c trycycler -n "__fish_seen_subcommand_from dotplot" -l "version" -d "Show program's version number and exit"
 
 
 
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -s "c" -l "cluster_dir" -d "Cluster directory (should contain a 1_contigs subdirectory)" -r
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -s "r" -l "reads" -d "Long reads (FASTQ format) used to generate the assemblies" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "linear" -d "The input contigs are not circular (default: assume the input contigs are circular)"
-complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "verbose" -d "Display extra output (for debugging) (default: False)"
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_mash_dist" -d "Maximum allowed pairwise Mash distance (default: 0.02)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_length_diff" -d "Maximum allowed pairwise relative length difference (default: 1.1)" -x
@@ -49,7 +58,7 @@ complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_add_seq
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_trim_seq" -d "Maximum allowed sequence length to be trimmed in order to fix circularisation (default: 50000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_trim_seq_percent" -d "Maximum allowed percent relative sequence length to be trimmed in order to fix circularisation (default: 10.0)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "min_identity" -d "Minimum allowed pairwise percent identity (default: 98.0)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_indel_size" -d "Maximum allowed pairwise indel size (default: 250)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "max_indel_size" -d "Maximum allowed pairwise indel size (default: 1000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from reconcile" -l "version" -d "Show program's version number and exit"
 
@@ -59,7 +68,7 @@ complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "c" -l "cluster_di
 complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "k" -l "kmer" -d "K-mer size used for sequence partitioning (default: 32)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "s" -l "step" -d "Step size used for sequence partitioning (default: 1000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "l" -l "lookahead" -d "Look-ahead margin used for sequence partitioning (default: 10000)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "t" -l "threads" -d "Number of threads to use for multiple sequence alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "t" -l "threads" -d "Number of threads to use for multiple sequence alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from msa" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from msa" -l "version" -d "Show program's version number and exit"
 
@@ -69,7 +78,7 @@ complete -c trycycler -n "__fish_seen_subcommand_from partition" -s "c" -l "clus
 complete -c trycycler -n "__fish_seen_subcommand_from partition" -s "r" -l "reads" -d "Long reads (FASTQ format) used to generate the assemblies" -x
 complete -c trycycler -n "__fish_seen_subcommand_from partition" -l "min_aligned_len" -d "Do not consider reads with less than this many bases aligned (default: 1000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from partition" -l "min_read_cov" -d "Do not consider reads with less than this percentages of their length covered by alignments (default: 90.0)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from partition" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from partition" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from partition" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from partition" -l "version" -d "Show program's version number and exit"
 
@@ -79,7 +88,7 @@ complete -c trycycler -n "__fish_seen_subcommand_from consensus" -s "c" -l "clus
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -l "linear" -d "The input contigs are not circular (default: assume the input contigs are circular)"
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -l "min_aligned_len" -d "Do not consider reads with less than this many bases aligned (default: 1000)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -l "min_read_cov" -d "Do not consider reads with less than this percentages of their length covered by alignments (default: 90.0)" -x
-complete -c trycycler -n "__fish_seen_subcommand_from consensus" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 4)" -x
+complete -c trycycler -n "__fish_seen_subcommand_from consensus" -s "t" -l "threads" -d "Number of threads to use for alignment (default: 12)" -x
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -l "verbose" -d "Display extra output (for debugging) (default: False)"
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -s "h" -l "help" -d "Show this help message and exit"
 complete -c trycycler -n "__fish_seen_subcommand_from consensus" -l "version" -d "Show program's version number and exit"
--- completions/unicycler.fish
+++ completions/unicycler.fish
@@ -3,15 +3,39 @@
 complete -c unicycler -s "h" -l "help" -d "Show this help message and exit"
 complete -c unicycler -l "help_all" -d "Show a help message with all program options"
 complete -c unicycler -l "version" -d "Show Unicycler's version number"
-complete -c unicycler -s "1" -l "short1" -d "FASTQ file of first short reads in each pair (required)" -r
-complete -c unicycler -s "2" -l "short2" -d "FASTQ file of second short reads in each pair (required)" -r
-complete -c unicycler -s "s" -l "unpaired" -d "FASTQ file of unpaired short reads (optional)" -r
-complete -c unicycler -s "l" -l "long" -d "FASTQ or FASTA file of long reads (optional)" -r
-complete -c unicycler -s "o" -l "out" -d "Output directory (required)" -r
-complete -c unicycler -l "verbosity" -d "Level of stdout and log file information (default: 1)   0 = no stdout, 1 = basic progress indicators,   2 = extra info, 3 = debugging info" -r
+complete -c unicycler -s "1" -l "short1" -d "FASTQ file of first short reads in each pair" -r
+complete -c unicycler -s "2" -l "short2" -d "FASTQ file of second short reads in each pair" -r
+complete -c unicycler -s "s" -l "unpaired" -d "FASTQ file of unpaired short reads" -r
+complete -c unicycler -s "l" -l "long" -d "FASTQ or FASTA file of long reads" -r
+complete -c unicycler -s "o" -l "out" -d "Output directory (required),--verbosity VERBOSITY   Level of stdout and log file information (default: 1)   0 = no stdout, 1 = basic progress indicators, 2 = extra info, 3 = debugging info" -r
+complete -c unicycler -l "keep" -d "Level of file retention (default: 1)   0 = only keep final files: assembly (FASTA, GFA and log), 1 = also save graphs at main checkpoints, 2 = also keep SAM (enables fast rerun in different mode), 3 = keep all temp files and save all graphs (for debugging)" -r
 complete -c unicycler -l "min_fasta_length" -d "Exclude contigs from the FASTA file which are shorter than this length (default: 100)" -r
-complete -c unicycler -l "keep" -d "Level of file retention (default: 1)   0 = only keep final files: assembly (FASTA,   GFA and log),   1 = also save graphs at main checkpoints,   2 = also keep SAM (enables fast rerun in different mode),   3 = keep all temp files and save all graphs (for debugging)" -r
-complete -c unicycler -l "vcf" -d "Produce a VCF by mapping the short reads to the final assembly (experimental, default: do not produce a vcf file)"
-complete -c unicycler -s "t" -l "threads" -d "Number of threads used (default: 4)" -x
-complete -c unicycler -l "mode" -d "Bridging mode (default: normal)   conservative = smaller contigs, lowest misassembly                  rate   normal = moderate contig size and misassembly rate   bold = longest contigs, higher misassembly rate" -x
+complete -c unicycler -s "t" -l "threads" -d "Number of threads used (default: 8)" -x
+complete -c unicycler -l "mode" -d "Bridging mode (default: normal)" -x
+complete -c unicycler -l "min_bridge_qual" -d "Do not apply bridges with a quality below this value" -x
 complete -c unicycler -l "linear_seqs" -d "The expected number of linear (i.e. non-circular) sequences in the underlying sequence (default: 0)" -x
+complete -c unicycler -l "min_anchor_seg_len" -d "If set, Unicycler will not use segments shorter than this as scaffolding anchors (default: automatic threshold)" -x
+complete -c unicycler -l "spades_path" -d "Path to the SPAdes executable (default: spades.py)" -r
+complete -c unicycler -l "min_kmer_frac" -d "Lowest k-mer size for SPAdes assembly, expressed as a fraction of the read length (default: 0.2)" -x
+complete -c unicycler -l "max_kmer_frac" -d "Highest k-mer size for SPAdes assembly, expressed as a fraction of the read length (default: 0.95)" -x
+complete -c unicycler -l "kmers" -d "Exact k-mers to use for SPAdes assembly, commaseparated (example: 21,51,71, default: automatic)" -x
+complete -c unicycler -l "kmer_count" -d "Number of k-mer steps to use in SPAdes assembly (default: 8)" -x
+complete -c unicycler -l "depth_filter" -d "Filter out contigs lower than this fraction of the chromosomal depth, if doing so does not result in graph dead ends (default: 0.25)" -x
+complete -c unicycler -l "largest_component" -d "Only keep the largest connected component of the assembly graph (default: keep all connected components)"
+complete -c unicycler -l "spades_options" -d "Additional options to be given to SPAdes (example: \" --phred-offset 33\", default: no additional options)" -x
+complete -c unicycler -l "no_miniasm" -d "Skip miniasm+Racon bridging (default: use miniasm and Racon to produce long-read bridges)"
+complete -c unicycler -l "racon_path" -d "Path to the Racon executable (default: racon)" -r
+complete -c unicycler -l "existing_long_read_assembly" -d "A pre-prepared long-read assembly for the sample in GFA or FASTA format." -x
+complete -c unicycler -l "no_simple_bridges" -d "Skip simple long-read bridging (default: use simple long-read bridging)"
+complete -c unicycler -l "no_long_read_alignment" -d "Skip long-read-alignment-based bridging (default: use long-read alignments to produce bridges)"
+complete -c unicycler -l "contamination" -d "FASTA file of known contamination in long reads" -r
+complete -c unicycler -l "scores" -d "Comma-delimited string of alignment scores: match, mismatch, gap open, gap extend (default: 3,-6,-5,-2)" -x
+complete -c unicycler -l "low_score" -d "Score threshold - alignments below this are considered poor (default: set threshold automatically)" -x
+complete -c unicycler -l "min_component_size" -d "Graph components smaller than this size (bp) will be removed from the final graph (default: 1000)" -x
+complete -c unicycler -l "min_dead_end_size" -d "Graph dead ends smaller than this size (bp) will be removed from the final graph (default: 1000)" -x
+complete -c unicycler -l "no_rotate" -d "Do not rotate completed replicons to start at a standard gene (default: completed replicons are rotated)"
+complete -c unicycler -l "start_genes" -d "FASTA file of genes for start point of rotated replicons (default: start_genes.fasta)" -r
+complete -c unicycler -l "start_gene_id" -d "The minimum required BLAST percent identity for a start gene search (default: 90.0)" -x
+complete -c unicycler -l "start_gene_cov" -d "The minimum required BLAST percent coverage for a start gene search (default: 95.0)" -x
+complete -c unicycler -l "makeblastdb_path" -d "Path to the makeblastdb executable (default: makeblastdb)" -r
+complete -c unicycler -l "tblastn_path" -d "Path to the tblastn executable (default: tblastn)" -r
