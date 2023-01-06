85da94822168ec4e3db8d6800aeeb8e6eb595f32
Yamato Matsuoka <yamaton@gmail.com>
2023-01-06 13:02:54 -0500

    Reflect updates in yamaton/h2o-curated-data/bio

---
+++ completions/faToTwoBit.fish
@@ -0,0 +1,6 @@
+# Auto-generated with h2o
+
+complete -c faToTwoBit -o "long" -d "use 64-bit offsets for index."
+complete -c faToTwoBit -o "noMask" -d "Ignore lower-case masking in fa file."
+complete -c faToTwoBit -o "stripVersion" -d "Strip off version number after '.' for GenBank accessions."
+complete -c faToTwoBit -o "ignoreDups" -d "Convert first sequence only if there are duplicate sequence names."
---
+++ completions/wigToBigWig.fish
@@ -0,0 +1,8 @@
+# Auto-generated with h2o
+
+complete -c wigToBigWig -o "blockSize" -d "Number of items to bundle in r-tree." -x
+complete -c wigToBigWig -o "itemsPerSlot" -d "Number of data points bundled at lowest level." -x
+complete -c wigToBigWig -o "clip" -d "If set just issue warning messages rather than dying if wig file contains items off end of chromosome or chromosomes that are not in the chrom.sizes file."
+complete -c wigToBigWig -o "unc" -d "If set, do not use compression."
+complete -c wigToBigWig -o "fixedSummaries" -d "If set, use a predefined sequence of summary levels."
+complete -c wigToBigWig -o "keepAllChromosomes" -d "If set, store all chromosomes in b-tree."
