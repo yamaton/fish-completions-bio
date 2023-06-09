9a01faee53e4885d3291c066dabbb33fc4f114e7
Yamato Matsuoka <yamaton@gmail.com>
2023-06-09 14:53:52 -0400

    Add kaiju

---
+++ completions/kaiju.fish
@@ -0,0 +1,17 @@
+# Auto-generated with h2o
+
+complete -c kaiju -s "t" -d "Name of nodes.dmp file" -r
+complete -c kaiju -s "f" -d "Name of database (.fmi) file" -r
+complete -c kaiju -s "i" -d "Name of input file containing reads in FASTA or FASTQ format" -r
+complete -c kaiju -s "j" -d "Name of second input file for paired-end reads" -r
+complete -c kaiju -s "o" -d "Name of output file." -r
+complete -c kaiju -s "z" -d "Number of parallel threads for classification (default: 1)" -x
+complete -c kaiju -s "a" -d "Run mode, either \"mem\" or \"greedy\" (default: greedy)" -x
+complete -c kaiju -s "e" -d "Number of mismatches allowed in Greedy mode (default: 3)" -x
+complete -c kaiju -s "m" -d "Minimum match length (default: 11)" -x
+complete -c kaiju -s "s" -d "Minimum match score in Greedy mode (default: 65)" -x
+complete -c kaiju -s "E" -d "Minimum E-value in Greedy mode (default: 0.01)" -x
+complete -c kaiju -s "x" -d "Enable SEG low complexity filter (enabled by default)"
+complete -c kaiju -s "X" -d "Disable SEG low complexity filter"
+complete -c kaiju -s "p" -d "Input sequences are protein sequences"
+complete -c kaiju -s "v" -d "Enable verbose output"
