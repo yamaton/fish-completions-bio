12f4be946fbccacd0a49b4ff94fd7919222e51f4
Yamato Matsuoka <yamaton@gmail.com>
2023-09-07 18:41:03 -0400

    Fix datatool

--- completions/datatool.fish
+++ completions/datatool.fish
@@ -1,2 +1,62 @@
 # Auto-generated with h2o
 
+complete -c datatool -s "m" -d "module file(s)" -r
+complete -c datatool -s "h" -d "Print USAGE and DESCRIPTION; ignore all other parameters"
+complete -c datatool -o "help" -d "Print USAGE, DESCRIPTION and ARGUMENTS; ignore all other parameters"
+complete -c datatool -o "xmlhelp" -d "Print USAGE, DESCRIPTION and ARGUMENTS in XML format; ignore all other"
+complete -c datatool -s "M" -d "external module file(s)" -r
+complete -c datatool -s "i" -d "ignore unresolved symbols"
+complete -c datatool -s "f" -d "write ASN.1 module file" -r
+complete -c datatool -o "fx" -d "write DTD file (\"-fx m\" writes modular DTD file)" -r
+complete -c datatool -o "fxs" -d "write XML Schema file (\"-fxs m\" writes modular Schema file)" -r
+complete -c datatool -o "fjs" -d "write JSON Schema file" -r
+complete -c datatool -o "fd" -d "write specification dump file (datatool internal format)" -r
+complete -c datatool -o "ms" -d "suffix of modular DTD or Schema file name" -r
+complete -c datatool -s "v" -d "read value in ASN.1 text format" -r
+complete -c datatool -o "vx" -d "read value in XML format" -r
+complete -c datatool -o "vj" -d "read value in JSON format" -r
+complete -c datatool -s "d" -d "read value in ASN.1 binary format (-t is required)" -r
+complete -c datatool -o "tvs" -d "read traversal specification file" -r
+complete -c datatool -s "t" -d "binary value type (see \"-d\" argument)" -x
+complete -c datatool -o "dn" -d "DTD module name in XML header (no extension)." -x
+complete -c datatool -s "F" -d "read value completely into memory"
+complete -c datatool -s "p" -d "write value in ASN.1 text format" -r
+complete -c datatool -o "px" -d "write value in XML format" -r
+complete -c datatool -o "pj" -d "write value in JSON format" -r
+complete -c datatool -o "xmlns" -d "XML namespace name" -x
+complete -c datatool -s "e" -d "write value in ASN.1 binary format" -r
+complete -c datatool -o "sxo" -d "no scope prefixes in XML output"
+complete -c datatool -o "sxi" -d "no scope prefixes in XML input"
+complete -c datatool -o "oex" -d "class export specifier for MSVC" -x
+complete -c datatool -o "od" -d "code definition file" -r
+complete -c datatool -o "odi" -d "silently ignore absent code definition file"
+complete -c datatool -o "odw" -d "issue a warning about absent code definition file"
+complete -c datatool -o "ods" -d "generate sample definition file"
+complete -c datatool -o "of" -d "write list of generated C++ files" -r
+complete -c datatool -o "oc" -d "write combining C++ files" -r
+complete -c datatool -o "oA" -d "generate C++ files for all types"
+complete -c datatool -o "ot" -d "generate C++ files for listed types" -r
+complete -c datatool -o "ox" -d "exclude listed types from generation" -x
+complete -c datatool -o "oX" -d "turn off recursive type generation"
+complete -c datatool -o "on" -d "default namespace" -x
+complete -c datatool -o "opm" -d "directory for searching source modules" -r
+complete -c datatool -o "oph" -d "directory for generated *.hpp files" -r
+complete -c datatool -o "opc" -d "directory for generated *.cpp files" -r
+complete -c datatool -o "or" -d "add prefix to generated file names" -r
+complete -c datatool -o "orq" -d "use quoted syntax form for generated include files"
+complete -c datatool -o "ors" -d "add source file dir to generated file names"
+complete -c datatool -o "orm" -d "add module name to generated file names"
+complete -c datatool -o "orA" -d "combine all -or* prefixes"
+complete -c datatool -o "ocvs" -d "create \".cvsignore\" files"
+complete -c datatool -o "oR" -d "set \"-o*\" arguments for NCBI directory tree" -r
+complete -c datatool -o "oDc" -d "turn on generation of DOXYGEN-style comments"
+complete -c datatool -o "odx" -d "URL of documentation root folder (for DOXYGEN)" -x
+complete -c datatool -o "lax_syntax" -d "allow non-standard ASN.1 syntax accepted by asntool"
+complete -c datatool -o "pch" -d "name of the precompiled header to include in all *.cpp files" -r
+complete -c datatool -o "logfile" -d "File to which the program log should be redirected" -r
+complete -c datatool -o "conffile" -d "Program's configuration (registry) data file" -r
+complete -c datatool -o "version" -d "Print version number; ignore other arguments"
+complete -c datatool -o "version-full" -d "Print extended version data; ignore other arguments"
+complete -c datatool -o "version-full-xml" -d "Print extended version data in XML format; ignore other arguments"
+complete -c datatool -o "version-full-json" -d "Print extended version data in JSON format; ignore other arguments"
+complete -c datatool -o "dryrun" -d "Dry run the application: do nothing, only test all preconditions"
