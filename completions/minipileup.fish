aef1556894952656223c365b3998e12cf95e5b8f
Yamato Matsuoka <yamaton@gmail.com>
2026-01-08 20:18:29 -0500

    Add minipileup

---
+++ completions/minipileup.fish
@@ -0,0 +1,20 @@
+# Auto-generated with h2o
+
+complete -c minipileup -s "f" -d "reference genome [null]" -r
+complete -c minipileup -s "v" -d "show variants only"
+complete -c minipileup -s "c" -d "output in the VCF format (force -v)"
+complete -c minipileup -s "C" -d "show count of each allele on both strands"
+complete -c minipileup -s "e" -d "use '*' to mark deleted bases"
+complete -c minipileup -s "y" -d "variant calling mode (-vcC -a2 -s5 -q30 -Q20)"
+complete -c minipileup -s "V" -d "print version number"
+complete -c minipileup -s "r" -d "region in format of 'ctg:start-end' [null]" -x
+complete -c minipileup -s "b" -d "BED or position list file to include [null]" -r
+complete -c minipileup -s "P" -d "only consider properly paired reads for paired-end reads"
+complete -c minipileup -s "q" -d "minimum mapping quality [0]" -x
+complete -c minipileup -s "l" -d "minimum alignment length [0]" -x
+complete -c minipileup -s "S" -d "minimum supplementary alignment length [0]" -x
+complete -c minipileup -s "Q" -d "minimum base quality [0]" -x
+complete -c minipileup -s "T" -d "skip bases within INT-bp from either end of a read [0]" -x
+complete -c minipileup -s "s" -d "drop alleles with depth<INT [1]" -x
+complete -c minipileup -s "a" -d "drop alleles with depth<INT on either strand [0]" -x
+complete -c minipileup -s "p" -d "drop an allele if the allele fraction is below FLOAT [0]" -x
