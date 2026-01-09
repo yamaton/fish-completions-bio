eac5e02cb3695478e33fe38298aa560dea1e0665
Yamato Matsuoka <yamaton@gmail.com>
2026-01-08 19:25:44 -0500

    Add longdust

---
+++ completions/longdust.fish
@@ -0,0 +1,11 @@
+# Auto-generated with h2o
+
+complete -c longdust -s "k" -d "k-mer length [7]" -x
+complete -c longdust -s "w" -d "window size [5000]" -x
+complete -c longdust -s "t" -d "score threshold [0.6]" -x
+complete -c longdust -s "g" -d "genome-wide GC content [0.5]" -x
+complete -c longdust -s "e" -d "extension X-drop length (0 to disable) [50]" -x
+complete -c longdust -s "s" -d "min start k-mer count (2 or 3) [3]" -x
+complete -c longdust -s "f" -d "forward strand only"
+complete -c longdust -s "a" -d "guaranteed O(Lw) algorithm but with more approximation"
+complete -c longdust -s "v" -d "version number"
